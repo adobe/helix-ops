f6084a4dc5b0e2e1462815b5479deae194ba0f20
rofe <github@rofe.com>
2019-12-05 15:02:46 +0100

    refactor(orb): reimplemented using node

---
+++ src/orb-release/cli.js
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2019 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/* eslint-disable no-param-reassign */
+
+const diff = require('diff');
+const fs = require('fs');
+const gitLogParser = require('git-log-parser');
+const getStream = require('get-stream');
+const shell = require('shelljs');
+const yargs = require('yargs');
+
+const releaseTypes = [
+  '',
+  'patch',
+  'minor',
+  'major',
+];
+const logger = console; // executed in CircleCI runtime
+let config = {};
+
+function getReleaseDate() {
+  if (fs.existsSync(config.changelog)) {
+    const cl = fs.readFileSync(config.changelog).toString('utf-8');
+    const match = /\((\d{4}-\d{2}-\d{2})\)/s.exec(cl);
+    if (match && match.length === 2) {
+      return new Date(match[1]);
+    }
+  }
+  return null;
+}
+
+async function getReleaseType() {
+  Object.assign(gitLogParser.fields, {
+    committerDate: { key: 'ci', type: Date },
+    message: 'B',
+  });
+  let type = 0;
+  const releaseDate = getReleaseDate();
+  if (releaseDate) {
+    logger.log('Last release date: %s', releaseDate.toUTCString());
+    (await getStream.array(gitLogParser.parse()))
+      .filter((commit) => {
+        if (new Date(commit.committerDate) >= releaseDate) {
+          if (/^(fix|perf)\(?.*\)?:/.test(commit.subject) && type < releaseTypes.indexOf('patch')) {
+            type = releaseTypes.indexOf('patch');
+          }
+          if (/^feat\(?.*\)?:/.test(commit.subject) && type < releaseTypes.indexOf('minor')) {
+            type = releaseTypes.indexOf('minor');
+          }
+          if (/.*(BREAKING CHANGE).*/.test(commit.message)) {
+            type = releaseTypes.indexOf('major');
+          }
+          return true;
+        }
+        return false;
+      });
+  } else {
+    logger.warn('Unable to determine last release date');
+  }
+  return releaseTypes[type];
+}
+
+function getOrbs() {
+  const orbs = [];
+  const dirs = fs.readdirSync(config.orbDir, { withFileTypes: true })
+    .filter((dir) => dir.isDirectory());
+  dirs.forEach((orb) => {
+    const dir = `${config.orbDir}/${orb.name}`;
+    orbs.push({
+      name: orb.name,
+      dir,
+      src: `${dir}/${config.orbSrc}`,
+      tmp: fs.mkdtempSync(`/tmp/${orb.name}-`),
+    });
+  });
+  return orbs;
+}
+
+function diffOrb(orb) {
+  let ret = false;
+  const pubFile = `${orb.tmp}/src.yml`;
+  shell.exec(`circleci orb source ${config.namespace}/${orb.name} > "${pubFile}"`);
+  if (shell.error()) {
+    return ret;
+  }
+  const pubSource = fs.readFileSync(pubFile).toString('utf-8');
+  const locSource = `${fs.readFileSync(orb.src).toString('utf-8')}\n`;
+  const patch = diff.diffTrimmedLines(pubSource, locSource);
+  patch.forEach((entry) => {
+    if (entry.added || entry.removed) {
+      ret = true;
+    }
+  });
+  return ret;
+}
+
+function releaseOrb(orb, type) {
+  return shell.exec(`circleci orb publish increment "${orb.src}" ${config.namespace}/${orb.name} ${type}`);
+}
+
+async function releaseOrbs(opts) {
+  config = {
+    ...opts,
+    ...config,
+  };
+  logger.log('Analyzing commits since last release');
+  const releaseType = await getReleaseType();
+  if (releaseType) {
+    logger.log('Proposed release type:', releaseType);
+    getOrbs().forEach((orb) => {
+      logger.log('Checking orb %s', orb.name);
+      if (diffOrb(orb)) {
+        logger.log('  Orb source changed, proceeding with %s release', releaseType);
+        releaseOrb(orb, releaseType);
+      } else {
+        logger.log('  Orb source unchanged, skipping release');
+      }
+    });
+  } else {
+    logger.log('No release');
+  }
+  logger.log('Done');
+}
+
+function baseargs(y) {
+  return y
+    .option('token', {
+      type: 'string',
+      alias: 'auth',
+      describe: 'CircleCI CLI Token (or env $CIRCLECI_CLI_TOKEN)',
+      required: true,
+    })
+    .option('namespace', {
+      type: 'string',
+      describe: 'The namespace to publish the orbs under',
+      default: 'adobe',
+      required: false,
+    })
+    .option('orb-dir', {
+      type: 'string',
+      alias: 'orbDir',
+      describe: 'The relative path to the orbs',
+      default: '.circleci/orbs',
+      required: false,
+    })
+    .option('orb-src', {
+      type: 'string',
+      alias: 'orbSrc',
+      describe: 'The name of the orb source file',
+      default: 'orb.yml',
+      required: false,
+    })
+    .option('changelog', {
+      type: 'string',
+      describe: 'The path to the change log file',
+      default: './CHANGELOG.md',
+      required: false,
+    });
+}
+
+class CLI {
+  // eslint-disable-next-line class-methods-use-this
+  run(arg) {
+    return yargs
+      .scriptName('orb-release')
+      .usage('$0')
+      .command('$0', 'default', (y) => baseargs(y), releaseOrbs)
+      .help()
+      .strict()
+      .env('CIRCLECI_CLI')
+      .parse(arg)
+      .argv;
+  }
+}
+
+module.exports = CLI;
---
+++ test/testOrbRelease.js
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2019 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/* eslint-env mocha */
+
+const assert = require('assert');
+const nock = require('nock');
+const shell = require('shelljs');
+const sinon = require('sinon');
+
+const OrbRelease = require('../src/orb-release/cli');
+
+function buildArgs({
+  auth, pageId: namespace, orbDir, orbSrc, changelog,
+} = {}) {
+  const args = [];
+  if (auth) args.push('--auth', auth);
+  if (namespace) args.push('--namespace', namespace);
+  if (orbDir) args.push('--name', `"${orbDir}"`);
+  if (orbSrc) args.push('--orb-src', `"${orbSrc}"`);
+  if (changelog) args.push('--changelog', `"${changelog}"`);
+  return args;
+}
+
+async function run(opts = {}) {
+  return new OrbRelease().run(buildArgs(opts));
+}
+
+async function runShell(opts = {}) {
+  return shell.exec(`node ./src/orb-release ${buildArgs(opts).join(' ')}`);
+}
+
+describe('Testing orb-release', () => {
+  const logger = console;
+
+  // defaults
+  const auth = 'fake-token';
+  const changelog = './test/fixtures/orb-release/changelog.md';
+  const changelogEmpty = './test/fixtures/orb-release/changelog-empty.md';
+  const orbSrc = '../../../test/fixtures/orb-release/src.yml';
+
+  // env variable backups
+  const originalAuth = process.env.CIRCLECI_CLI_TOKEN;
+
+  before(() => {
+    sinon.spy(logger, 'log');
+    sinon.spy(logger, 'error');
+    // make sure there are no env variables around for this test
+    delete process.env.CIRCLECI_CLI_TOKEN;
+  });
+
+  beforeEach(() => {
+    nock.restore();
+    nock.cleanAll();
+    nock.activate();
+  });
+
+  after(() => {
+    // unwrap the sinon spies
+    logger.log.restore();
+    logger.error.restore();
+    // add env variables back
+    process.env.CIRCLECI_CLI_TOKEN = originalAuth;
+  });
+
+  it('refuses to run without required arguments', async () => {
+    const output = await runShell({});
+    assert.notEqual(output.code, 0, `expected exit code != 0, but got ${output.code}`);
+    assert.ok(/Missing required argument: token/.test(output.stderr), 'expected missing required arguments');
+  }).timeout(5000);
+
+  it('skips release if orb source unchanged', async () => {
+    const output = await runShell({
+      auth,
+      changelog,
+    });
+    assert.equal(output.code, 0, `expected exit code 0, but got ${output.code}`);
+    assert.ok(/Orb source unchanged, skipping release/.test(output.stdout), 'expected to skip release if orb source unchanged');
+  }).timeout(5000);
+
+  it('skips release if no release date', async () => {
+    const output = await runShell({
+      auth,
+      changelog: changelogEmpty,
+    });
+    assert.equal(output.code, 0, `expected exit code 0, but got ${output.code}`);
+    assert.ok(/Unable to determine last release date/.test(output.stderr), 'expected to skip release if no release date');
+  }).timeout(5000);
+
+  it('attempts orb release', async () => {
+    const output = await runShell({
+      auth,
+      changelog,
+      orbSrc,
+    });
+    assert.equal(output.code, 0, `expected exit code 0, but got ${output.code}`);
+    assert.ok(/User does not have access to publish SemVer orbs in this namespace/.test(output.stderr), 'expected to attempt release');
+  }).timeout(5000);
+
+  it('uses environment variables', async () => {
+    process.env.CIRCLECI_CLI_TOKEN = auth;
+    const output = await runShell({
+      changelog,
+    });
+    assert.equal(output.code, 0, `expected exit code 0, but got ${output.code}`);
+    assert.ok(/Orb source unchanged, skipping release/.test(output.stdout), 'expected to skip release if orb source unchanged');
+    delete process.env.CIRCLECI_CLI_TOKEN;
+  }).timeout(5000);
+});
