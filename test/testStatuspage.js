5b24d48b70c89c0be531db1e5ff6e50173a0179c
Stefan Guggisberg <sguggisb@adobe.com>
2020-07-01 15:15:02 +0200

    test: fix test setup for fetch & nock

--- test/testStatuspage.js
+++ test/testStatuspage.js
@@ -16,11 +16,11 @@ const assert = require('assert');
 const shell = require('shelljs');
 const sinon = require('sinon');
 
+process.env.HELIX_FETCH_FORCE_HTTP1 = 'true';
+
 const Statuspage = require('../src/statuspage/cli');
 const StatuspageAPI = require('./statuspage/StatuspageAPI');
 const { getTimedPromise } = require('./utils');
-
-process.env.HELIX_FETCH_FORCE_HTTP1 = 'true';
 
 function buildArgs({
   cmd, auth, pageId, name, desc, group, incubator, incubatorPageId, silent,
